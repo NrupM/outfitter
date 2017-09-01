c4139aa83d422c9887f520635415ef5429cc2396
NrupM <maryenorthrup@gmail.com>
2017-09-01 12:01:16 -0700

    create all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
 
   get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: "new_user"
+  get '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 
   get '/items', to: 'items#index', as: 'items'
+  get '/items/new', to: 'items#new', as: "new_item"
+  get '/items', to: 'items#create'
+  get '/items/:id', to: 'items#show', as: 'item'
+  get '/items/:id/edit', to: 'items#edit', as: 'edit_item'
+  patch '/items/:id', to: 'items#update'
+  delete '/items/:id', to: 'items#destroy'
+
 
   get '/outfits', to: 'outfits#index', as: 'outfits'
+  get '/outfits/new', to: 'outfits#new', as: "new_outfit"
+  get '/outfits', to: 'outfits#create'
+  get '/outfits/:id', to: 'outfits#show', as: 'outfit'
+  get '/outfits/:id/edit', to: 'outfits#edit', as: 'edit_outfit'
+  patch '/outfits/:id', to: 'outfits#update'
+  delete '/outfits/:id', to: 'outfits#destroy'
 
 end
