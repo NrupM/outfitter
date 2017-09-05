50f9cdc5af303b69f711cc4a3550d818135af287
NrupM <maryenorthrup@gmail.com>
2017-09-05 14:45:48 -0700

    render outfits on outfit index view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require carousel
 //= require rails-ujs
-//= require turbolinks
 //= require_tree .
